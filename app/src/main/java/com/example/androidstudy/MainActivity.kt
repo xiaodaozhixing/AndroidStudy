e8c50e30b82a539d4f67e06866825b3fc75a1951
chenjili <AuspiciousChan@163.com>
2019-08-19 00:54:58 +0800

    feat($DataBinding): 配置DataBinding

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.androidstudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.androidstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val x = 1.0
+        print(x.addNumber(11))
     }
+}
+
+private fun Double.addNumber(num:Number):Double{
+    return this + num.toDouble()
 }
