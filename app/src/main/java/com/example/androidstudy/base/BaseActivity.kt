c91da55faeaa9919df817bab9a22841a80ffe208
Auspicious Chan <auspiciouschan@163.com>
2019-08-20 00:52:55 +0800

    fix($BaseActivity): 使用只有一个参数的onCreate

--- app/src/main/java/com/example/androidstudy/base/BaseActivity.kt
+++ app/src/main/java/com/example/androidstudy/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidstudy.base
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -10,8 +11,8 @@ import com.example.androidstudy.base.presenter.BasePresenter
 abstract class BaseActivity<T:BasePresenter<*>, B: ViewDataBinding>: AppCompatActivity() {
     protected lateinit var mBinding: B
     protected lateinit var mPresenter: T
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         mPresenter = getPresenter()
         mBinding = DataBindingUtil.setContentView(this, getLayoutId())
     }
